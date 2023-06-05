eec7e330884d5a794a5f88712346beb161efb42b
nkc20236035 <nkc23.etou.hiroto@gmail.com>
2023-06-05 15:19:15 +0900

    6月5日更新

--- Assets/textControllre.cs
+++ Assets/textControllre.cs
@@ -7,16 +7,16 @@ public class textControllre : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0.01f,0,0);
-        if (transform.position.x >48)
+        if(transform.position.x > 48)
         {
-            transform.position = new Vector3(0,0,0);
+            transform.position = new Vector3(0, 0, 0);
         }
     }
 }
